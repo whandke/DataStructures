96c1dd0cec6b5867f9fca383ddbdc08e9b060c7a
whandke <handkewojciech@gmail.com>
2017-04-03 11:46:41 +0200

    Koniec generatorów

--- src/ArrayGenerator.java
+++ src/ArrayGenerator.java
@@ -2,17 +2,22 @@ import java.util.Random;
 
 public class ArrayGenerator {
 	
-	public static void random(int[] array, int maximum, float range){			
-		for( int j = 0; j < maximum; j++){
-			Random rn = new Random();
-			float n = range * maximum;
-			int a = rn.nextInt((int)n) + 1;
-			array[j] = + a;
+	private static void swap(int[] array, int a, int b){
+		int temp = array[a];
+		array[a] = array[b];
+		array[b] = temp;		
+	}
+	
+	public static void random(int[] array){	
+		int length = array.length;
+		Random rn = new Random();
+		for(int i = 0; i < length; i++){
+			array[i] = rn.nextInt(length);
 		}
 	}
 
-	public static void rising(int[] array, int maximum){
-		for(int i = 0; i < maximum; i++)
+	public static void rising(int[] array){
+		for(int i = 0; i < array.length; i++)
 			array[i] = i + 1;
 	}
 	
@@ -30,6 +35,22 @@ public class ArrayGenerator {
 			array[i + lenght] = lenght - i;
 		for(int i = 0; i < lenght; i++)
 			array[i] = i + 1;
+	}
+	
+	public static void generate(int[] array){
+		int length = array.length;
+		int[] rising = new int[length];
+		int[] random = new int[length];
+		
+		rising(rising);
+		random(random);
+		
+		for(int i = 0; i < length; i++)
+			swap(rising, i, random[i]);
+		for(int i = 0; i < length; i++)
+			array[i] = rising[i];
+		
+		
 	}
 
 }
